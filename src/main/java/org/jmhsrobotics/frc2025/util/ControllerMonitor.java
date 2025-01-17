1132374928a1c10e98b7537de71e8307869e1301
Thomas Santonicola <thomas.santonicola@gmail.com>
2025-01-16 20:44:02 -0500

    Added Base Framework for Controller Disconnected Alert
    WIP, not working, reason unknown

--- src/main/java/org/jmhsrobotics/frc2025/util/ControllerMonitor.java
+++ src/main/java/org/jmhsrobotics/frc2025/util/ControllerMonitor.java
@@ -6,16 +6,29 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class ControllerMonitor {
-  private static Map<XboxController, Alert> controllermap = new HashMap<XboxController, Alert>();
+  private static Map<XboxController, Alert[]> controllermap =
+      new HashMap<XboxController, Alert[]>();
 
   public static void checkController() {
     for (XboxController control : controllermap.keySet()) {
-      controllermap.get(control).set((control.getAxisCount() == 6));
+      if (control.isConnected()) {
+        controllermap.get(control)[0].set((control.getAxisCount() == 6));
+        controllermap.get(control)[1].set(false);
+      } else {
+
+        controllermap.get(control)[0].set(false);
+        controllermap.get(control)[1].set(true);
+      }
+      System.out.println("is connected: " + control.isConnected());
     }
   }
 
   public static void addController(XboxController c, String a) {
-
-    controllermap.put(c, new Alert("Wrong Controller Mode: " + a, Alert.AlertType.kError));
+    controllermap.put(
+        c,
+        new Alert[] {
+          new Alert("Wrong Controller Mode: " + a, Alert.AlertType.kError),
+          new Alert("Controller Disconnected: " + a, Alert.AlertType.kWarning)
+        });
   }
 }

6e914c1fcf982890a5aa8e526a44e46006357d45
npwtub <145060985+npwtub@users.noreply.github.com>
2025-01-30 23:18:30 -0500

    Added basic wrist subsystem, command, wristIO, and neowristIO

---
+++ src/main/java/org/jmhsrobotics/frc2025/subsystems/wrist/Wrist.java
@@ -0,0 +1,25 @@
+package org.jmhsrobotics.frc2025.subsystems.wrist;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Wrist extends SubsystemBase {
+  private WristIO wristIO;
+  private WristIOInputsAutoLogged inputs = new WristIOInputsAutoLogged();
+
+  public Wrist(WristIO wristIO) {
+    this.wristIO = wristIO;
+  }
+
+  @Override
+  public void periodic() {
+    wristIO.updateInputs(inputs);
+  }
+
+  public boolean atGoal(double setPoint) {
+    return inputs.positionDegrees == setPoint;
+  }
+
+  public void setSetpoint(double setPoint) {
+    wristIO.setPositionDegrees(setPoint);
+  }
+}
---
+++ src/main/java/org/jmhsrobotics/frc2025/subsystems/wrist/WristIO.java
@@ -0,0 +1,16 @@
+package org.jmhsrobotics.frc2025.subsystems.wrist;
+
+import org.littletonrobotics.junction.AutoLog;
+
+public interface WristIO {
+  @AutoLog
+  public static class WristIOInputs {
+    public double positionDegrees;
+    public double motorRPM;
+    public double motorAmps;
+  }
+
+  public default void updateInputs(WristIOInputs inputs) {}
+
+  public default void setPositionDegrees(double angle) {}
+}

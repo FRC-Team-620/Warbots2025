09bf8acd05349f369d11e160764a512698abb17b
npwtub <noahweintraub1@gmail.com>
2025-03-01 13:55:32 -0500

    SysId drivetrain tuning, fixing npe,

--- src/main/java/org/jmhsrobotics/frc2025/subsystems/drive/DriveConstants.java
+++ src/main/java/org/jmhsrobotics/frc2025/subsystems/drive/DriveConstants.java
@@ -22,7 +22,7 @@ import edu.wpi.first.math.util.Units;
 
 public class DriveConstants {
 
-  public static final double maxSpeedMetersPerSec = 4.8768;
+  public static final double maxSpeedMetersPerSec = 4.8768 * 0.7;
   public static final double odometryFrequency = 100.0; // Hz
 
   public static final Rotation2d frontLeftZeroRotation = new Rotation2d(0.0);
@@ -182,7 +182,8 @@ public class DriveConstants {
     public static final double turnEncoderVelocityFactor = (2 * Math.PI) / 60.0; // RPM -> Rad/Sec
 
     // Turn PID configuration
-    public static final double turnKp = 0.5;
+    // 14 for p value was too much
+    public static final double turnKp = 7;
     public static final double turnKd = 0.0;
     public static final double turnSimP = 0.2;
     public static final double turnSimD = 0.0;

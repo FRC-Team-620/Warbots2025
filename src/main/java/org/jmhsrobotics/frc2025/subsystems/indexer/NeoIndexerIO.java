0743c8882eb65ef852b7c89d7dfb9001df8b27cc
m10653 <m10653@users.noreply.github.com>
2025-03-05 17:12:40 -0500

    still

--- src/main/java/org/jmhsrobotics/frc2025/subsystems/indexer/NeoIndexerIO.java
+++ src/main/java/org/jmhsrobotics/frc2025/subsystems/indexer/NeoIndexerIO.java
@@ -1,4 +1,4 @@
-package org.jmhsrobotics.frc2025.subsystems.climber.indexer;
+package org.jmhsrobotics.frc2025.subsystems.indexer;
 
 import com.revrobotics.AbsoluteEncoder;
 import com.revrobotics.spark.SparkBase.ControlType;
@@ -7,6 +7,7 @@ import com.revrobotics.spark.SparkBase.ResetMode;
 import com.revrobotics.spark.SparkClosedLoopController;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.config.ClosedLoopConfig.FeedbackSensor;
 import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 import com.revrobotics.spark.config.SparkMaxConfig;
 import org.jmhsrobotics.frc2025.Constants;
@@ -14,21 +15,37 @@ import org.jmhsrobotics.frc2025.util.SparkUtil;
 
 public class NeoIndexerIO implements IndexerIO {
   private SparkMax motor = new SparkMax(Constants.CAN.kIndexerMotorID, MotorType.kBrushless);
-  private AbsoluteEncoder encoder;
   private SparkMaxConfig motorConfig = new SparkMaxConfig();
 
-  private SparkClosedLoopController pidController;
+  private AbsoluteEncoder absoluteEncoder = motor.getAbsoluteEncoder();
 
+  private SparkClosedLoopController pidController;
   private double setPointDegrees;
 
   public NeoIndexerIO() {
     motorConfig
         .idleMode(IdleMode.kBrake)
-        .smartCurrentLimit(40)
+        .smartCurrentLimit(12)
         .voltageCompensation(12)
         .inverted(false)
         .closedLoop
-        .outputRange(-0.25, 0.25);
+        .outputRange(-0.5, 0.5)
+        .pid(
+            Constants.IndexerConstants.kP,
+            Constants.IndexerConstants.kI,
+            Constants.IndexerConstants.kD)
+        .feedbackSensor(FeedbackSensor.kAbsoluteEncoder);
+    motorConfig.absoluteEncoder.positionConversionFactor(360).inverted(true);
+
+    motorConfig
+        .signals
+        .absoluteEncoderVelocityAlwaysOn(true)
+        .absoluteEncoderPositionAlwaysOn(true)
+        .absoluteEncoderPositionPeriodMs(20)
+        .absoluteEncoderVelocityPeriodMs(20)
+        .appliedOutputPeriodMs(20)
+        .busVoltagePeriodMs(20)
+        .outputCurrentPeriodMs(20);
 
     SparkUtil.tryUntilOk(
         motor,
@@ -37,14 +54,13 @@ public class NeoIndexerIO implements IndexerIO {
             motor.configure(
                 motorConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters));
 
-    encoder = motor.getAbsoluteEncoder();
     pidController = motor.getClosedLoopController();
   }
 
   @Override
   public void updateInputs(IndexerIOInputs inputs) {
-    SparkUtil.ifOk(motor, encoder::getPosition, (value) -> inputs.positionDegrees = value);
-    SparkUtil.ifOk(motor, encoder::getPosition, (value) -> inputs.positionDegrees = value);
+    SparkUtil.ifOk(motor, absoluteEncoder::getPosition, (value) -> inputs.positionDegrees = value);
+    SparkUtil.ifOk(motor, absoluteEncoder::getVelocity, (value) -> inputs.motorRPM = value);
     SparkUtil.ifOk(motor, motor::getOutputCurrent, (value) -> inputs.motorAmps = value);
 
     pidController.setReference(setPointDegrees, ControlType.kPosition);

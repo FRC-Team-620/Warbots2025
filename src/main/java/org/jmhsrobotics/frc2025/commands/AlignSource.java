b8893c42848227d4373f95c27fb7b3333fdb41fb
npwtub <noahweintraub1@gmail.com>
2025-03-18 20:47:06 -0400

    almost ready

--- src/main/java/org/jmhsrobotics/frc2025/commands/AlignSource.java
+++ src/main/java/org/jmhsrobotics/frc2025/commands/AlignSource.java
@@ -2,33 +2,36 @@ package org.jmhsrobotics.frc2025.commands;
 
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Pose3d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Transform2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
-import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import edu.wpi.first.wpilibj2.command.Command;
 import org.jmhsrobotics.frc2025.subsystems.drive.Drive;
+import org.jmhsrobotics.frc2025.subsystems.vision.VisionConstants;
 import org.littletonrobotics.junction.Logger;
 
 public class AlignSource extends Command {
   private Drive drive;
   private Pose2d goalPose;
-  private boolean alignCloseToSource;
+  private boolean alignCloseToStation;
 
   private final PIDController xController = new PIDController(0.6, 0, 0);
   private final PIDController yController = new PIDController(0.6, 0, 0);
-  private final PIDController thetaController = new PIDController(0.005, 0, 0);
+  private final PIDController thetaController = new PIDController(0.01, 0, 0);
 
-  public AlignSource(Drive drive, boolean alignCloseToSource) {
+  public AlignSource(Drive drive, boolean alignCloseToStation) {
     this.drive = drive;
-    this.alignCloseToSource = alignCloseToSource;
+    this.alignCloseToStation = alignCloseToStation;
 
     addRequirements(drive);
   }
 
   @Override
   public void initialize() {
-    this.goalPose = calculateReefSetpoints(drive, alignCloseToSource);
+    this.goalPose = calculateSetpoints(drive, alignCloseToStation);
     xController.reset();
     yController.reset();
     thetaController.reset();
@@ -40,7 +43,7 @@ public class AlignSource extends Command {
 
   @Override
   public void execute() {
-    this.goalPose = calculateReefSetpoints(drive, alignCloseToSource);
+    this.goalPose = calculateSetpoints(drive, alignCloseToStation);
     drive.runVelocity(
         ChassisSpeeds.fromFieldRelativeSpeeds(
             calculateSourceAutoAlignSpeeds(
@@ -78,20 +81,33 @@ public class AlignSource extends Command {
    * @return double array with index 0 being the x coordinate setpoint and index 1 being the y
    *     coordinate setpoint
    */
-  public static Pose2d calculateReefSetpoints(Drive drive, boolean alignCloseToSource) {
-    if (drive.getPose().getY() < 4) {
-      if (alignCloseToSource) {
-        return new Pose2d(0.62, 1.25, new Rotation2d(Units.degreesToRadians(54)));
+  public static Pose2d calculateSetpoints(Drive drive, boolean alignCloseToSource) {
+    Pose2d targetPose = new Pose2d();
+    if (DriverStation.getAlliance().orElse(Alliance.Blue) == Alliance.Red) {
+      if (drive.getPose().getY() > 4) {
+        Pose2d targetTagPose =
+            VisionConstants.aprilTagLayout.getTagPose(2).orElse(new Pose3d()).toPose2d();
+        if (alignCloseToSource) {
+          targetPose = targetTagPose.plus(new Transform2d(0.4, 0.5, new Rotation2d()));
+        } else {
+          targetPose = targetTagPose.plus(new Transform2d(0.4, -0.5, new Rotation2d()));
+        }
       } else {
-        return new Pose2d(1.55, 0.56, new Rotation2d(Units.degreesToRadians(54)));
+        Pose2d targetTagPose =
+            VisionConstants.aprilTagLayout.getTagPose(1).orElse(new Pose3d()).toPose2d();
       }
     } else {
-      if (alignCloseToSource) {
-        return new Pose2d(0.62, 6.75, new Rotation2d(Units.degreesToRadians(-54)));
+      if (drive.getPose().getY() > 4) {
+        Pose2d targetTagPose =
+            VisionConstants.aprilTagLayout.getTagPose(13).orElse(new Pose3d()).toPose2d();
       } else {
-        return new Pose2d(1.55, 7.45, new Rotation2d(Units.degreesToRadians(-54)));
+        Pose2d targetTagPose =
+            VisionConstants.aprilTagLayout.getTagPose(12).orElse(new Pose3d()).toPose2d();
       }
     }
+    // if (alignCloseToSource)
+    //   return targetTagPose.plus(new Transform2d(0.15, 0.61, new Rotation2d()));
+    return targetPose;
   }
 
   /**

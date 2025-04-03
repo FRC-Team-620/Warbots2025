c2070c7c669a68daec01b11f4feb21767fda30d0
npwtub <145060985+npwtub@users.noreply.github.com>
2025-04-03 14:36:20 -0400

    added led feedback for when coral is in indexer. ready for testing

--- src/main/java/org/jmhsrobotics/frc2025/RobotContainer.java
+++ src/main/java/org/jmhsrobotics/frc2025/RobotContainer.java
@@ -423,6 +423,12 @@ public class RobotContainer {
             Commands.run(
                     () -> led.setPattern(LEDPattern.solid(Color.kCyan).blink(Seconds.of(0.1))), led)
                 .withTimeout(1.5));
+
+    new Trigger(indexer::hasCoral)
+        .onTrue(
+            Commands.run(
+                    () -> led.setPattern(LEDPattern.solid(Color.kPeachPuff).blink(Seconds.of(0.1))), led)
+                .withTimeout(0.5));
   }
 
   private void setupSmartDashbaord() {

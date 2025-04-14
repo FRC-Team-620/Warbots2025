c40a2a4655cbc1f14a33c2bd16b5bfb8d3c690b1
m10653 <m10653@users.noreply.github.com>
2025-04-14 09:25:45 -0400

    Rough profiled align reef

---
+++ src/main/java/org/jmhsrobotics/frc2025/commands/autoAlign/AlignReefSetAngleProf.java
@@ -0,0 +1,198 @@
+package org.jmhsrobotics.frc2025.commands.autoAlign;
+
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.geometry.Pose3d;
+import edu.wpi.first.math.geometry.Rotation3d;
+import edu.wpi.first.math.geometry.Transform2d;
+import edu.wpi.first.math.geometry.Transform3d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.DriverStation.Alliance;
+import edu.wpi.first.wpilibj.LEDPattern;
+import edu.wpi.first.wpilibj2.command.Command;
+import org.jmhsrobotics.frc2025.subsystems.drive.Drive;
+import org.jmhsrobotics.frc2025.subsystems.elevator.Elevator;
+import org.jmhsrobotics.frc2025.subsystems.led.LED;
+import org.jmhsrobotics.frc2025.subsystems.vision.Vision;
+import org.jmhsrobotics.frc2025.util.ProfiledPIDControllerCustom;
+import org.jmhsrobotics.frc2025.util.TrapezoidProfileCustom.Constraints;
+import org.littletonrobotics.junction.Logger;
+
+public class AlignReefSetAngleProf extends Command {
+  private final Drive drive;
+  private final Vision vision;
+  private final LED led;
+  private final Elevator elevator;
+
+  // private final PIDController xController = new PIDController(0.525, 0, 0.01);
+  // private final PIDController yController = new PIDController(0.525, 0, 0.01);
+  private final ProfiledPIDControllerCustom distController =
+      new ProfiledPIDControllerCustom(0.525, 0, 0.01, new Constraints(100, 4.0, 2));
+  private final PIDController thetaController = new PIDController(0.01, 0, 0);
+
+  private double thetaGoalDegrees = 0; // Janky only work for one angle now
+
+  private LEDPattern progressPattern;
+  private double initialDistance = 3;
+  private double currentDistance = 3;
+  private boolean hasReset = false;
+
+  private int targetTagId;
+  private Pose3d lastTagPose = null;
+  private Transform2d goalTransform;
+  private Pose3d tagPose;
+
+  // boolean for if bot should align left or right
+  private boolean isLeft = true;
+
+  public AlignReefSetAngleProf(
+      Drive drive, Vision vision, LED led, Elevator elevator, boolean isLeft, int targetTagId) {
+    this.drive = drive;
+    this.vision = vision;
+    this.led = led;
+    this.elevator = elevator;
+    this.isLeft = isLeft;
+    this.targetTagId = targetTagId;
+
+    progressPattern =
+        LEDPattern.progressMaskLayer(
+            () -> ((this.initialDistance - this.currentDistance) / this.initialDistance));
+
+    addRequirements(drive);
+  }
+
+  private void adjustTagId() {
+    if (DriverStation.getAlliance().orElse(Alliance.Red) == Alliance.Red) {
+      switch (this.targetTagId) {
+        case 17:
+          this.targetTagId = 8;
+          break;
+        case 18:
+          this.targetTagId = 7;
+          break;
+        case 19:
+          this.targetTagId = 6;
+          break;
+        case 20:
+          this.targetTagId = 11;
+          break;
+        case 21:
+          this.targetTagId = 10;
+          break;
+        case 22:
+          this.targetTagId = 9;
+          break;
+      }
+    }
+  }
+
+  private double calculateGoalAngleFromId(int targetTagId) {
+    if (DriverStation.getAlliance().orElse(Alliance.Blue) == Alliance.Blue) {
+      if (targetTagId == 18) return 0;
+      else if (targetTagId == 17) return 60;
+      else if (targetTagId == 22) return 120;
+      else if (targetTagId == 21) return 180;
+      else if (targetTagId == 19) return -60;
+      else return -120;
+    }
+
+    // if current alliance is red, use the following angles
+    if (targetTagId == 7) return 180;
+    else if (targetTagId == 8) return -120;
+    else if (targetTagId == 10) return 0;
+    else if (targetTagId == 6) return 120;
+    else if (targetTagId == 9) return -60;
+    else return 60;
+  }
+
+  @Override
+  public void initialize() {
+    this.hasReset = false;
+    thetaController.reset();
+    thetaController.enableContinuousInput(-180, 180);
+
+    this.goalTransform = AutoAlign.calculateReefTransform(this.elevator.getSetpoint(), isLeft);
+    adjustTagId();
+    this.thetaGoalDegrees = calculateGoalAngleFromId(this.targetTagId);
+  }
+
+  @Override
+  public void execute() {
+    // New Reef Auto Align should have:
+    // calculates goal transform. recalculated in execute because the elevator height can change
+    // while it is running[\]
+
+    this.goalTransform = AutoAlign.calculateReefTransform(this.elevator.getSetpoint(), isLeft);
+
+    // gets target tag pose relative to bot
+    tagPose =
+        AutoAlign.getTagPoseRobotRelative(
+            this.targetTagId, this.vision, this.lastTagPose, this.drive.getPose());
+    // new Transform3d(this.goalTransform.getX(), this.goalTransform.getY(), 0,
+    // this.goalTransform.getRotation()
+    tagPose =
+        tagPose.plus(
+            new Transform3d(
+                this.goalTransform.getX(),
+                this.goalTransform.getY(),
+                -tagPose.getZ(),
+                new Rotation3d(this.goalTransform.getRotation())));
+    if (tagPose != null) {
+
+      lastTagPose =
+          new Pose3d(drive.getPose())
+              .plus(new Transform3d(tagPose.getTranslation(), tagPose.getRotation()));
+
+      this.currentDistance =
+          new Pose3d(drive.getPose()).getTranslation().getDistance(lastTagPose.getTranslation());
+      if (!hasReset) {
+        this.distController.reset(currentDistance);
+        this.hasReset = true;
+      }
+
+      // calculate chassis speeds based on tag pose relative to bot and goal transformation from
+      // that tag pose
+      ChassisSpeeds outputSpeeds = new ChassisSpeeds();
+
+      double speedScalar = distController.calculate(currentDistance, 0);
+      Translation2d direction =
+          drive
+              .getPose()
+              .getTranslation()
+              .minus(new Translation2d(lastTagPose.getX(), lastTagPose.getY()));
+      direction = direction.div(direction.getNorm()); // Convert to Unit Vector
+      direction = direction.times(speedScalar);
+      outputSpeeds = new ChassisSpeeds(direction.getX(), direction.getY(), 0);
+      // outputSpeeds =
+      // AutoAlign.getReefAlignSpeeds(
+      //     tagPose, this.goalTransform, this.xController, this.yController);
+      // calculate theta speeds
+      outputSpeeds =
+          outputSpeeds.plus(
+              AutoAlign.getAutoAlignThetaSpeeds(
+                  thetaController, this.thetaGoalDegrees, drive.getRotation()));
+
+      Logger.recordOutput("Align Reef/Target Tag Pose1", lastTagPose);
+      Logger.recordOutput("Align Reef/Target Tag ID1", this.targetTagId);
+      Logger.recordOutput("Align Reef/Target Angle1", this.thetaGoalDegrees);
+      Logger.recordOutput("Align Reef/Distance From Reef", this.currentDistance);
+
+      // calculates the distance from target for the LED progress pattern
+
+      if (currentDistance > Units.inchesToMeters(1)) drive.runVelocity(outputSpeeds);
+      // led.setPattern(progressPattern);
+    }
+  }
+
+  @Override
+  public boolean isFinished() {
+    return this.currentDistance < Units.inchesToMeters(1.5);
+  }
+
+  @Override
+  public void end(boolean interrupted) {
+    drive.stop();
+  }
+}
